975da461a0ece6f57c421c78595049b6d4e31553
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-08-02 10:16:42 +0200

    feat: add nocache URL option to public assets
    adding ?nocache to a public URL skips the cache headers

--- index.mjs
+++ index.mjs
@@ -63,7 +63,10 @@ async function onFetch(request, response) {
   }
 
   const extension = path.split(".").pop();
-  response.setHeader("Cache-Control", "max-age=86400");
+  if (!url.searchParams.has('nocache')) {
+    response.setHeader("Cache-Control", "max-age=86400");
+  }
+  
   response.setHeader("Access-Control-Allow-Origin", "*");
   response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST");
   response.setHeader("Content-Type", mimeTypes[extension] || "text/plain");
